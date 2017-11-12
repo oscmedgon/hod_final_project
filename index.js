9a01851f6369bee9261d98c9083b4049e963cd7b
oscraker <oscraker@localhost.localdomain>
2017-11-12 16:12:52 +0100

    Starting db

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
+// Dependencies
 const express = require('express')
 const bodyParser = require('body-parser')
 const moment = require('moment')
-require('dotenv').load()
-const app = express()
-var port = process.env.PORT || 8080
-const routes = require('./routes')
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
 
+// Load enviroment
+require('dotenv').load()
+
+// Loading database settings
+const urlDb = process.env.HOD_DB
+
+// Inicializating express server
+const app = express()
+
+// Starting Database connection
+mongoose.connect(urlDb, { useMongoClient: true })
+
+// Setting server port
+var port = process.env.PORT || 8080
+
+// Loading router
+const client = require('./routes/client')
+// const api = require('./routes/api')
+
+// Setting template engine
 app.set('view engine', 'pug')
+
+// Setting time manager
 app.locals.moment = moment
 
+// Setting statics
 app.use(express.static('public'))
 
+// Parsing body requests
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use(routes)
+// Calling routes
+app.use(client)
+// app.use(api)
 
+// Starting express server
 app.listen(port)
 console.log(`Listening on PORT ${port}...`)

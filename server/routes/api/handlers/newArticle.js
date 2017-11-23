2d8c7e972964dc96599b4d758fcdb86f404b19f3
Oscar Medina <oscmedgon@gmail.com>
2017-11-23 17:23:16 +0100

    handling administration login

--- server/routes/api/handlers/newArticle.js
+++ server/routes/api/handlers/newArticle.js
@@ -1,17 +1,23 @@
 const Article = require('../../../models/Article')
+const User = require('../../../models/User')
 const moment = require('moment')
 
 function newArticle (req, res) {
   const { title, category, featured, body } = req.body
   const date_of_creation = Date.now()
   const date_pretty = moment().format('DD-MM-YYYY, HH:mm:ss')
-  const author = '5a0c63eb7e766f4b2a302be0'
+  const author = req.user._id
   const image = req.body.image || 'https://thumb1.shutterstock.com/display_pic_with_logo/1072949/278165597/stock-photo-stamp-default-in-red-over-white-background-278165597.jpg'
   const article = new Article({title, category, featured, body, date_of_creation, date_pretty, author, image})
-
+  const {_id} = article
   article.save(article, err => {
     if (err) res.status(400).json({msg: 'Error creating article'})
-    else res.status(200).json({msg: 'Article succesfully created!'})
+    else {
+      User.findByIdAndUpdate(author, {
+        $addToSet: {'articles': {_id}}
+      }, {'new': true})
+      .then(res.status(200).json({msg: 'Article succesfully created!'}))
+    }
   })
 }
 

6c92118fdb88e9518cf1e5233d585d9d5f9fb90e
oscmedgon <oscmedgon@gmail.com>
2017-11-26 19:12:24 +0100

    More informaton for user dashboard

--- server/routes/api/handlers/userDashboard.js
+++ server/routes/api/handlers/userDashboard.js
@@ -1,12 +1,19 @@
 const User = require('../../../models/User')
+const moment = require('moment')
 
 function userDashboard (req, res) {
   User.find()
   .then(users => {
     const json = {}
+    json.recentUsers = users.filter(user => {
+      let timeSinzeRegistration = moment(user.date_of_creation, 'DD-MM-YYYY, hh:mm:ss').fromNow().split(' ')[0]
+      if (timeSinzeRegistration === 'a') timeSinzeRegistration = 1
+      return (parseInt(timeSinzeRegistration) <= 7)
+    }).length
     json.totalUsers = users.length
     json.totalAdmins = (users.filter(user => user.user_type === 1)).length
     json.userList = users
+    console.log(json)
     res.status(200).json(json)
   })
 }

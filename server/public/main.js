738794fd2c81b2dde95aa83c6772b7c43b601401
oscmedgon <oscmedgon@gmail.com>
2017-12-18 10:10:40 +0100

    Security improvements forcing HTTPS

--- server/public/main.js
+++ server/public/main.js
@@ -1,3 +1,4 @@
+if (window.location.protocol === 'http') window.location.protocol = 'https'
 $('.message a').click(function () {
   $('form').animate({height: 'toggle', opacity: 'toggle'}, 'slow')
 })

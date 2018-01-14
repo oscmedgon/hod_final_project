9813631137f3895f564a7e24f10560a1de8999a0
oscmedgon <oscmedgon@gmail.com>
2018-01-14 15:28:03 +0100

    Securing discord link

--- server/routes/api/handlers/signDiscordLink.js
+++ server/routes/api/handlers/signDiscordLink.js
@@ -1,10 +1,20 @@
+const User = require('../../../models/User')
 const jwt = require('jsonwebtoken')
 const { SECRET } = process.env
 
-function signDiscordLink (req, res) {
+async function signDiscordLink (req, res) {
   const { id, username } = req.body
-  const token = jwt.sign({ id, username }, SECRET)
-  res.status(200).json({token: token})
+  const data = await User.find({discord:
+  { id: id,
+    status: true,
+    username: username}
+  })
+  if (!data.length) {
+    const token = jwt.sign({ id, username }, SECRET)
+    res.status(200).json({token: token})
+  } else {
+    res.status(400).json({msg: 'Esta cuenta de discord ya ha sido vinculada con anterioridad, contacte con el servicio técnico si tiene problemas.'})
+  }
 }
 
 module.exports = signDiscordLink

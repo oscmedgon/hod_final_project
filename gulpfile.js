84909203f2b6a83563b665286f9f6d1edfae45c7
Oscar Medina <oscmedgon@gmail.com>
2018-04-02 04:46:19 +0200

    gulp implementations

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,59 @@ const sass = require('gulp-sass');
 const sassBundler = require('gulp-sass-bundler');
 const clean = require('gulp-clean');
 const replace = require('gulp-replace');
+const watch = require('gulp-watch');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const minify = require('gulp-minify');
 
 const sassInput = './server/views/raw/sass';
-const sassOutput = './server/views/raw/sass/bundle';
-const output = './server/public/styles/bundle';
+const jsInput = './server/views/raw/scripts/bundle.js';
+const sassOutput = './server/views/raw/bundle';
+const cssOutput = './server/public/styles/bundle';
+const jsOutput = './server/public/scripts';
 var hash = '';
 
-var sassOptions = {
-  errLogToConsole: true,
-  outputStyle: 'compressed'
-};
+gulp.task('jsMin', ['clean-js', 'scripts-templates'], function () {
+  gulp.src(jsInput)
+    .pipe(minify({
+      ext: {
+        src: `.${hash}.debug.js`,
+        min: `.${hash}.js`
+      },
+      exclude: ['tasks'],
+      ignoreFiles: ['.combo.js', '-min.js']
+    }))
+    .pipe(gulp.dest(jsOutput));
+});
 
-gulp.task('sass', ['clean-sass', 'clean-css', 'bundle-sass', 'templates'], function () {
+gulp.task('clean-js', function () {
+  return gulp.src(`${jsOutput}/*`, {read: false})
+    .pipe(clean());
+});
+
+gulp.task('scripts-templates', function () {
+  hash = Date.now() / 1000 | 0;
+  gulp.src(['./server/views/layout/scripts.pug'])
+    .pipe(replace(/bundle.[0-9]{1,10}.js/, `bundle.${hash}.js`))
+    .pipe(gulp.dest('./server/views/layout'));
+});
+
+
+gulp.task('sass', ['clean-sass', 'clean-css', 'bundle-sass', 'styles-templates'], function () {
+  const sassOptions = {
+    errLogToConsole: true,
+    outputStyle: 'compressed'
+  };
+  const autoprefixerOptions = {
+    browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+  };
   return gulp
-    .src('./server/views/raw/sass/bundle/*.scss')
+    .src('./server/views/raw/bundle/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass(sassOptions).on('error', sass.logError))
-    .pipe(gulp.dest(output));
+    .pipe(sourcemaps.write(`bundle.${hash}.map.css`))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(cssOutput));
 });
 
 gulp.task('bundle-sass', function () {
@@ -39,12 +76,19 @@ gulp.task('clean-sass', function () {
     .pipe(clean());
 });
 gulp.task('clean-css', function () {
-  return gulp.src(`${output}/*`, {read: false})
+  return gulp.src(`${cssOutput}/*`, {read: false})
     .pipe(clean());
 });
 
-gulp.task('templates', function () {
+gulp.task('styles-templates', function () {
   gulp.src(['./server/views/layout/styles.pug'])
     .pipe(replace(/bundle.[0-9]{1,10}.css/, `bundle.${hash}.css`))
     .pipe(gulp.dest('./server/views/layout'));
 });
+
+gulp.task('watch', function () {
+  gulp.watch(`${sassInput}/**/*.scss`, ['sass']);
+  gulp.watch(jsInput, ['jsMin']);
+});
+
+gulp.task('prod', ['sass', 'jsMin']);
---
+++ server/views/raw/scripts/bundle.js
@@ -0,0 +1,127 @@
+$('.message a').click(function () {
+  $('form').animate({height: 'toggle', opacity: 'toggle'}, 'slow');
+});
+console.log('Welcome to harbingers of Devastation');
+$('.register-form').on('submit', function (e) {
+  e.preventDefault();
+  const data = {
+    username: e.target[0].value.toLowerCase(),
+    password: e.target[1].value,
+    email: e.target[2].value
+  };
+  const url = '/api_register';
+  const method = 'POST';
+  const {username, password} = data;
+  $.ajax({ url, method, data })
+    .then(response => {
+      toastr['success'](response.msg);
+      const data = {
+        username: username,
+        password: password
+      };
+      const url = '/api_login';
+      const method = 'POST';
+      $.ajax({ url, method, data })
+        .then(response => {
+          toastr['success'](response.msg);
+          window.location.reload();
+        }, response => {
+          toastr['error']('El usuario o la contraseña son incorrectos.');
+        });
+    }, response => {
+      toastr['error']('Error al crear el usuario, es posible que el usuario ya exista.');
+    });
+});
+
+$('.login-form').on('submit', function (e) {
+  e.preventDefault();
+  const data = {
+    username: e.target[0].value.toLowerCase(),
+    password: e.target[1].value
+  };
+  const url = '/api_login';
+  const method = 'POST';
+  $.ajax({ url, method, data })
+    .then(response => {
+      toastr['success'](response.msg);
+      window.location.reload();
+    }, response => {
+      toastr['error']('El usuario o la contraseña son incorrectos.');
+    });
+});
+
+$('.input-file').on('change', function (e) {
+  e.preventDefault();
+  const id = $(this).parent().data('id');
+  let data = new FormData();
+  let file = e.target.files[0];
+  // add the files to formData object for the data payload
+  data.append('file', file);
+  const url = `/user/${id}/modify/avatar`;
+  axios.post(url, data)
+    .then(response => {
+      toastr['success']('Avatar modificado correctamente');
+      window.location.reload();
+    }, response => {
+      toastr['error']('Ha habido un problema al actualizar su avatar, intentelo de nuevo más tarde.');
+    });
+});
+
+$('.userDataModify').on('submit', function (e) {
+  e.preventDefault();
+  const id = $(this).data('id');
+  const data = {
+    name: e.target[0].value,
+    description: e.target[1].value,
+    website: e.target[2].value
+  };
+  const url = `/user/${id}/modify/data`;
+  const method = 'POST';
+  $.ajax({ url, method, data })
+    .then(response => {
+      toastr['success'](response.msg);
+      window.location.reload();
+    }, response => {
+      toastr['error']('Ha habido un problema al actualizar su información, es posible que el usuario ya esté en uso..');
+    });
+});
+$('#file').change(function () {
+  var filepath = this.value;
+  var m = filepath.match(/([^\/\\]+)$/);
+  var filename = m[1];
+  $('#file-upload-label').text('Subiendo... ' + filename);
+});
+
+$('.link-discord').on('click', e => {
+  const data = {
+    token: e.target.id
+  };
+  const url = '/api/discord/link';
+  const method = 'POST';
+  $.ajax({ url, method, data })
+    .then(response => {
+      toastr['success'](response.msg);
+      $('#root').text(response.msg);
+    }, response => {
+      toastr['error'](response.msg);
+      $('#root').text(response.msg);
+    });
+});
+
+$('.coment-form').on('submit', e => {
+  e.preventDefault();
+  const data = {
+    article: e.target.id,
+    title: e.target[0].value,
+    body: e.target[1].value
+  };
+  const url = '/api/coment/add';
+  const method = 'POST';
+  $.ajax({ url, method, data })
+    .then(response => {
+      toastr['success'](response.msg);
+      window.location.reload();
+    }, response => {
+      toastr['error'](response.msg);
+    });
+});

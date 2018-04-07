baea0093ee91efd184e9d64b2b7963c361925282
Oscar <oscmedgon@gmail.com>
2018-04-07 12:49:40 +0200

    fixed placeholder image and some styles

--- server/routes/api/handlers/newArticle.js
+++ server/routes/api/handlers/newArticle.js
@@ -7,7 +7,7 @@ function newArticle (req, res) {
   const date_of_creation = Date.now()
   const date_pretty = moment().format('DD-MM-YYYY, HH:mm:ss')
   const author = req.user._id
-  const image = req.body.image || 'http://res.cloudinary.com/dm303fk5u/image/upload/v1511966508/silueta_fondo_transparente-min_yjgaol.png'
+  const image = req.body.image || 'https://res.cloudinary.com/dm303fk5u/image/upload/v1519556344/xntno4pmavzs6rjn8szp.png'
   const article = new Article({title, category, featured, body, date_of_creation, date_pretty, author, image})
   const {_id} = article
   article.save(article, err => {

04e4b627e86735b4a6e57c3e442e193d7b9e9660
Óscar Medina <oscmedgon@gmail.com>
2018-09-09 01:58:02 +0200

    Dividing coments from articles

--- server/routes/client/handlers/getArticle.js
+++ server/routes/client/handlers/getArticle.js
@@ -1,26 +1,27 @@
 const Article = require('../../../models/Article')
-const Coment = require('../../../models/Coment')
 const User = require('../../../models/User')
-const getUserData = require('./getUserData')
+
 
 function getArticle (req, res) {
     const userData = req.user
     const {id} = req.params
     Article.find({_id: id})
     .then(article => User.populate(article, {path: 'author'}, function (err, article) {
-        if (err) res.status(400).json(err)
-        Coment.find({article: id})
-        .then(coments => User.populate(coments, {path: 'author'}, function (err, coments) {
-            if (err) res.status(400).json(err)
-            res.status(200).json({
-                data: {
-                    ...article[0]._doc,
-                    coments
-                },
-                msg: 'Featured articles fetch success'
-            })
-        }))
+        console.log(`FETCH ARTICLE BY ID ${id} OK` )
+        console.log(article)
+        res.status(200).json({
+            data: {
+                ...article[0]._doc
+            },
+            msg: 'Éxito buscando el artículo solicitado'
+        })
     }))
+    .catch((error) => {
+        res.status(404).json({
+            error,
+            msg: 'Error en la solicitud, el artículo que buscas no existe'
+        })
+    })
 }
 
 module.exports = getArticle
